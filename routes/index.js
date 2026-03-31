2a46ed2610dcde6752fb01c34e8cf16b001cb0b4
Ojeaga <ojeagabclifford@gmail.com>
2026-03-31 07:49:16 +0100

    work out

--- routes/index.js
+++ routes/index.js
@@ -8,15 +8,15 @@ routes.use('/movies', moviesRoutes);
 
 routes.get('/login', passport.authenticate('github'), (req,res)=>{});
 
-
+// I use AI to clearlify this code there where no req.session destory before i was trying to logout i notice it did not work so i was try to find
+//better way to see it work out
 routes.get('/logout', (req, res, next) => {
   // 1. Tell Passport to log out
   req.logout((err) => {
     if (err) {
       return next(err);
     }
-    
-    // 2. Destroy the Express session to clear req.session.user
+  
     req.session.destroy((err) => {
       // 3. Redirect to home page
       res.redirect('/');
